c0374ca3450d0cee13f5839d795f107bd5a96a1c
SergeySeroff <harek20@mail.ru>
2022-07-22 20:42:43 +0300

    add Example 15 ready

---
+++ Example15/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите число от 1 до 7: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if(n1==1) Console.Write("нет");
+if(n1==2) Console.Write("нет");
+if(n1==3) Console.Write("нет");
+if(n1==4) Console.Write("нет");
+if(n1==5) Console.Write("нет");
+if(n1==6) Console.Write("да");
+if(n1==7) Console.Write("да");
+if(n1<1) Console.Write("введите число от 1 до 7");
+if(n1>7) Console.Write("введите число от 1 до 7");
