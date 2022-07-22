221025cdfad53945f03189a71c92ddf43316cb0f
SergeySeroff <harek20@mail.ru>
2022-07-22 20:30:09 +0300

    add Example 13 ready 2.0

---
+++ Example13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+Console.Write("Введите число ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+while(n1 < 99) 
+{
+    Console.WriteLine("третьей цифры нет");
+    return;
+}
+    
+while (n1 > 999)
+{
+    n1 = n1 / 10;
+}
+int result = n1 % 10;
+
+Console.Write(result);
